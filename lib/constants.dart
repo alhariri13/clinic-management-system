1e546e32eddfb058f9adae8ace5607f02f7d9108
alhariri13 <abood.al.hariri@gamil.com>
2026-07-20 19:20:36 +0300

    connect the login screen to the backend and new look for it

--- lib/constants.dart
+++ lib/constants.dart
@@ -4,7 +4,7 @@ import 'package:flutter/material.dart';
 enum UserRole { patient, doctor }
 
 class Constants {
-  static const String baseUrl = 'http://192.168.1.107:8000';
+  static const String baseUrl = 'http://192.168.1.107:8000/api/';
   static const Color appColor = Color(0xFF011D34);
   static String token = '';
   static Dio dio = Dio();
